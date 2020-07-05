4312380c8648b34ea5271d29701e25d3936007bf
JuanAgosta20 <50495331+JuanAgosta20@users.noreply.github.com>
2020-07-05 13:58:57 -0300

    Informe Ok

--- Application_TP_L5_GRUPO_3/src/main/webapp/Resources/js/Charts.js
+++ Application_TP_L5_GRUPO_3/src/main/webapp/Resources/js/Charts.js
@@ -1,46 +1,4 @@
-var ctx = document.getElementById('chartContainer').getContext('2d');
-var myChart = new Chart(ctx, {
-	type : 'line',
-	data : {
-		labels : [ 'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-				'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre',
-				'Diciembre' ],
-		datasets : [ {
-			label : '# de cuotas abonadas por mes',
-			data : [ 12, 19, 3, 5, 2, 3, 30, 15, 9, 6, 8, 0 ],
-			backgroundColor : [ 'rgba(255, 0, 0, 0.2)',
-					'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
-					'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)',
-					'rgba(255, 159, 64, 0.2)' ],
-			borderColor : [ 'rgba(255, 0, 0, 1)', 'rgba(54, 162, 235, 1)',
-					'rgba(255, 0, 0, 1)', 'rgba(75, 192, 192, 1)',
-					'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)' ],
-			borderWidth : 2
-		} ]
-	},
-	options : {
-		scales : {
-			yAxes : [ {
-				ticks : {
-					beginAtZero : true
-				}
-			} ]
-		}
-	}
-});
-
-async function buscarDatos(){
-	await fetch('getTransfers.do?init='+'end='+)
-	.then(response => response.json())
-	.then(data => {
-			console.log(data);
-			//actualizarChart(data);
-		})
-	})
-}
-
-function actualizarChart(datos){
-	var ctx = document.getElementById('chartContainer').getContext('2d');
+/*	var ctx = document.getElementById('chartContainer').getContext('2d');
 	var myChart = new Chart(ctx, {
 		type : 'line',
 		data : {
@@ -49,7 +7,58 @@ function actualizarChart(datos){
 					'Diciembre' ],
 			datasets : [ {
 				label : '# de cuotas abonadas por mes',
-				data : [ datos[0], datos[1], datos[2], datos[3], datos[4], datos[5], datos[6], datos[7], datos[8], datos[9], datos[10], datos[11]],
+				data : [ 12, 19, 3, 5, 2, 3, 30, 15, 9, 6, 8, 0 ],
+				backgroundColor : [ 'rgba(255, 0, 0, 0.2)',
+						'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
+						'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)',
+						'rgba(255, 159, 64, 0.2)' ],
+				borderColor : [ 'rgba(255, 0, 0, 1)', 'rgba(54, 162, 235, 1)',
+						'rgba(255, 0, 0, 1)', 'rgba(75, 192, 192, 1)',
+						'rgba(153, 102, 255, 1)', 'rgba(255, 159, 64, 1)' ],
+				borderWidth : 2
+			} ]
+		},
+		options : {
+			scales : {
+				yAxes : [ {
+					ticks : {
+						beginAtZero : true
+					}
+				} ]
+			}
+		}
+	});*/
+
+	
+	async function buscarDatos(){
+	let init = document.getElementById('init').value;
+	let end = document.getElementById('end').value;
+	let months = new Array();
+	let quantity = new Array();;
+	await fetch('getTransfers.do?init='+init+'&end='+end)
+	.then(response => response.json())
+	.then(data => {
+			console.log(data);
+			data.forEach(e =>{
+				months.push(e.monthName);
+				quantity.push(e.quantity);
+			})
+			// console.log(months);
+			// console.log(quantity);
+			actualizarChart(months, quantity);
+		})
+}
+
+
+function actualizarChart(months, quantity){
+	var ctx = document.getElementById('chartContainer').getContext('2d');
+	var myChart = new Chart(ctx, {
+		type : 'bar',
+		data : {
+			labels : months,
+			datasets : [ {
+				label : '# de transacciones por mes.',
+				data : quantity,
 				backgroundColor : [ 'rgba(255, 0, 0, 0.2)',
 						'rgba(54, 162, 235, 0.2)', 'rgba(255, 206, 86, 0.2)',
 						'rgba(75, 192, 192, 0.2)', 'rgba(153, 102, 255, 0.2)',
--- Application_TP_L5_GRUPO_3/src/main/webapp/WEB-INF/views/admReports.jsp
+++ Application_TP_L5_GRUPO_3/src/main/webapp/WEB-INF/views/admReports.jsp
@@ -11,9 +11,12 @@
 %>
 <html>
 <head>
-
+<script
+	src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
+<script src="Resources/js/Charts.js"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
 <jsp:include page="Include.html"></jsp:include>
+
 <title>ARVO Bank - Reportes</title>
 </head>
 <body>
@@ -25,24 +28,46 @@
 
 			<div class="col-2">
 				<!-- Columna vacía izquierda-->
-				<select style="margin-top: 30px">
+				<!-- <select style="margin-top: 30px" >
 					<option>Seleccione una opción</option>
 					<option>Cuotas abonadas por mes</option>
-				</select> <br></br> <label label-for="desde">Desde: </label> <input
-					id="desde" type="date"></input> <br></br> <label label-for="hasta">Hasta:
-				</label> <input id="hasta" type="date"></input>
+				</select> -->
+				<br></br> <label label-for="desde">Desde: </label></br> <input id="init"
+					type="date" onchange="activarEnd()"></input> <br></br> <label
+					label-for="hasta">Hasta:</label></br> <input id="end" type="date"
+					disabled onchange="buscarDatos()"></input>
 			</div>
 			<div class="col-10">
 				<canvas id="chartContainer" style="height: 370px; width: 100%;"></canvas>
 			</div>
 		</div>
 	</div>
-</body>
-<script
-	src="https://cdn.jsdelivr.net/npm/chart.js@2.9.3/dist/Chart.min.js"></script>
-<script src="Resources/js/Charts.js"></script>
-<script>
 
+</body>
+
+<script>
+	function activarEnd() {
+		let init = document.getElementById('init');
+		let end = document.getElementById('end');
+		end.disabled = false;
+		let date = new Date(init.value);
+		let year;
+		if (!!date.valueOf()) {
+			year = date.getFullYear();
+			month = date.getMonth() + 1;
+			day = date.getDate();
+			//console.log(year+' '+month+' '+ day);
+			if (month === 12 && day == 31) {
+				year = year + 1;
+				//console.log(year+'act');
+			}
+		}
+		end.max = year + '-12-31';
+		end.min = init.value;
+
+	}
+	
+	
 </script>
 
 </html>
